0d48cf7af8041939d29e249318ed8152168fcecd
Park Donghyeon <emma415g@gmail.com>
2021-12-09 02:54:03 +0900

    simple capture, windows & osx transparent

--- main.py
+++ main.py
@@ -1,4 +1,5 @@
 import os, sys
+# For OSX
 if getattr(sys, 'frozen', False):
     _path = sys.executable
 else:
@@ -6,10 +7,15 @@ else:
 
 os.chdir(os.path.dirname(os.path.abspath(_path)))
 
+import platform
+os_dict = {'W': 'WINDOWS', 'D': 'OSX', 'L': 'LINUX'}
+OS_NAME = os_dict.get(platform.platform()[0], 'UNKNOWN')
+
 import tkinter as tk
 import threading
 import glob
 from sslog.logger import SimpleLogger
+from PIL import ImageGrab
 from functools import partial
 from selenium import webdriver
 from selenium.webdriver.chrome.service import Service
@@ -47,9 +53,14 @@ def open_browser():
     threading.Thread(target=task).start()
 
 
-def on_text():
+def on_btn(_root):
     global driver
-    log.info(driver.title)
+    # log.info(driver.title)
+    print(root.winfo_geometry())
+
+
+def on_capture(_canvas):
+    ImageGrab.grab().crop((100,100,300,300)).save('abcd.png')
 
 
 def on_destroy(_root):
@@ -67,16 +78,27 @@ def on_destroy(_root):
 if __name__ == "__main__":
     root = tk.Tk()
     root.wm_attributes('-topmost', True)  # Top only
-    root.wm_attributes('-transparent', True)  # Allowed transparent
+    if OS_NAME == 'WINDOWS':
+        root.wm_attributes('-transparentcolor', 'white')  # Allowed transparent
 
     root.title("Web Controller")
     root.geometry("480x320+100+100")
-    # root.attributes('-alpha', 1)
     root.resizable(True, True)
+
+    ####################################################################
+    screen_frame = tk.Frame(root, bg='red')
+    screen_frame.pack(side=tk.LEFT, expand=True, fill='both')
+
+    if OS_NAME == 'WINDOWS':
+        canvas = tk.Canvas(screen_frame, bg='white')
+    elif OS_NAME == 'OSX':
+        canvas = tk.Canvas(screen_frame, bg='systemTransparent')
+
+    canvas.pack(expand=True, fill='both', padx=5, pady=5)
 
     ########################### Button Frame ###########################
     btn_frame = tk.Frame(root)
-    btn_frame.pack(side=tk.LEFT, )
+    btn_frame.pack(side=tk.LEFT)
 
     btn_frame_opt = {
         'master': btn_frame,
@@ -84,22 +106,15 @@ if __name__ == "__main__":
     }
     btn_pack_opt = {
         'pady': 10,
+        'fill': 'both'
     }
     btn_open = tk.Button(**btn_frame_opt, text='Open', command=open_browser)
     btn_open.pack(btn_pack_opt)
-    btn_start = tk.Button(**btn_frame_opt, text='start', command=open_browser)
+    btn_start = tk.Button(**btn_frame_opt, text='start', command=partial(on_btn, root))
     btn_start.pack(btn_pack_opt)
-    btn_stop = tk.Button(**btn_frame_opt, text='stop')
+    btn_stop = tk.Button(**btn_frame_opt, text='stop', command=partial(on_capture, canvas))
     btn_stop.pack(btn_pack_opt)
     ####################################################################
 
-    screen_frame = tk.Frame(root, bg='red')
-    screen_frame.pack(side=tk.LEFT, expand=True, fill='both')
-
-    canvas = tk.Canvas(screen_frame, bg='systemTransparent')
-    canvas.pack(expand=True, fill='both', padx=5, pady=5)
-
-
-    ####################################################################
     root.protocol("WM_DELETE_WINDOW", partial(on_destroy, root))
     root.mainloop()
